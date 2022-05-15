6d791ad3feb62af7dbd9442391bfed9fa88f8452
Narima <47755927+Karambamba@users.noreply.github.com>
2022-05-15 09:48:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function start() {
-	//По нажатию на кнопку наш таймер начнет работать:
 	window.setInterval(timer, 50);
 }
 
-//Эта функция будет запускаться каждую секунду
 function timer() {
 	var elem = document.getElementById('test');
-    elem.value = parseInt(elem.value)-1; //parseInt преобразует строку в число
+    elem.value = parseInt(elem.value)-1;
 }
